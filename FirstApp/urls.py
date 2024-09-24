dfd9b8a49298480e56dc3ab1ffb600f85400811a
olgaSterkhova <omsterkhova@gmail.com>
2024-09-24 19:23:25 +0700

    new correct

--- FirstApp/urls.py
+++ FirstApp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from MainApp.views import views
+from MainApp import views
 
 urlpatterns = [
   #  path('test/',test),
